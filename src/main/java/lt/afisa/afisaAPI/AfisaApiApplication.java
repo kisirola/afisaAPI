4a42d7acc361a11311b92f207bc39b3abc11d54d
kisirola <kisirola@gmail.com>
2019-10-01 18:54:04 +0300

    Initial commition

---
+++ src/main/java/lt/afisa/afisaAPI/AfisaApiApplication.java
@@ -0,0 +1,13 @@
+package lt.afisa.afisaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfisaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfisaApiApplication.class, args);
+	}
+
+}
